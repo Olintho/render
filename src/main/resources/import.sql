3ebdd966cde35c76fe2a4a7c7ecfdb939856b443
Olintho Jesus <olinthojesus@yahoo.com.br>
2026-02-02 21:32:13 -0300

    Modifying Import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO BOOKS (code, description, name) VALUES (200000, 'The book guide', 'Read the book');
-INSERT INTO BOOKS (code, description, name) VALUES (200001, 'Java Man, the definitive guide aboit java developers', 'Java Man');
-INSERT INTO BOOKS (code, description, name) VALUES (200002, 'scrito por uma das maiores referências da programação, Barry Burd, o Java Para Leigos, Tradução da 5ª Edição, traz informações atualizadas sobre linguagens de programação, o básico da programação', 'Java para leigos');
-INSERT INTO BOOKS (code, description, name) VALUES (200003, 'Java: Como Programar', 'Java: Como Programar');
-INSERT INTO BOOKS (code, description, name) VALUES (200004, 'Use a Cabeça! Java é uma experiência completa de aprendizado em programação orientada a objetos (OO) e Java. Projetado de acordo com princípios de aprendizado simples, este livro mostrará de aspectos', 'Use a Cabeça');
+INSERT INTO BOOKS (code, description, name) VALUES (300000, 'The book guide', 'Read the book');
+INSERT INTO BOOKS (code, description, name) VALUES (300001, 'Java Man, the definitive guide aboit java developers', 'Java Man');
+INSERT INTO BOOKS (code, description, name) VALUES (300002, 'scrito por uma das maiores referências da programação, Barry Burd, o Java Para Leigos, Tradução da 5ª Edição, traz informações atualizadas sobre linguagens de programação, o básico da programação', 'Java para leigos');
+INSERT INTO BOOKS (code, description, name) VALUES (300003, 'Java: Como Programar', 'Java: Como Programar');
+INSERT INTO BOOKS (code, description, name) VALUES (300004, 'Use a Cabeça! Java é uma experiência completa de aprendizado em programação orientada a objetos (OO) e Java. Projetado de acordo com princípios de aprendizado simples, este livro mostrará de aspectos', 'Use a Cabeça');
